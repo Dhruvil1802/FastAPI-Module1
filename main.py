8d2506b5876ba1a0887c54b802c58c6bdd2b3eba
Dhruvil1802 <dhruvilpatel180201@gmail.com>
2024-09-18 16:40:55 -0400

    added error handling, request files and request forms and files

--- main.py
+++ main.py
@@ -1,10 +1,32 @@
 from datetime import datetime, time, timedelta
 from enum import Enum
-from typing import Literal, Optional, Union
+from typing import Literal, Union
 from uuid import UUID
 
-from fastapi import Body, Cookie, FastAPI, Form, Header, Query, Path
-from pydantic import BaseModel, EmailStr, Field, HttpUrl
+from fastapi import (
+    Body,
+    FastAPI,
+    Query,
+    Path,
+    Cookie,
+    Header,
+    status,
+    Form,
+    File,
+    UploadFile,
+    HTTPException,
+    Request,
+)
+from fastapi.encoders import jsonable_encoder
+from fastapi.exceptions import RequestValidationError
+from fastapi.exception_handlers import (
+    http_exception_handler,
+    request_validation_exception_handler,
+)
+from fastapi.responses import JSONResponse, PlainTextResponse
+from pydantic import BaseModel, Field, HttpUrl, EmailStr
+from starlette.exceptions import HTTPException as StarletteHTTPException
+from starlette.responses import HTMLResponse
 
 app = FastAPI()
 
@@ -431,7 +453,7 @@ async def read_items():
 async def get_arbitrary():
     return {"foo": 1, "bar": "2"}
 
-#Form
+# Form
 
 @app.post("/login/")
 async def login(username: str = Form(...), password: str = Body(...)):
@@ -443,3 +465,73 @@ async def login(username: str = Form(...), password: str = Body(...)):
 async def login_json(username: str = Body(...), password: str = Body(...)):
     print("password", password)
     return {"username": username}
+
+upload files
+
+@app.post("/files/")
+async def create_file(
+    files: list[bytes] = File(..., description="A file read as bytes")
+):
+    return {"file_sizes": [len(file) for file in files]}
+
+
+@app.post("/uploadfile/")
+async def create_upload_file(
+    files: list[UploadFile] = File(..., description="A file read as UploadFile")
+):
+    return {"filename": [file.filename for file in files]}
+
+error handling
+
+items = {"foo": "The Foo Wrestlers"}
+
+
+@app.get("/items/{item_id}")
+async def read_item(item_id: str):
+    if item_id not in items:
+        raise HTTPException(
+            status_code=404,
+            detail="Item not found",
+            headers={"X-Error": "There goes my error"},
+        )
+    return {"item": items[item_id]}
+
+
+class UnicornException(Exception):
+    def __init__(self, name: str):
+        self.name = name
+
+
+@app.exception_handler(UnicornException)
+async def unicorn_exception_handler(request: Request, exc: UnicornException):
+    return JSONResponse(
+        status_code=418,
+        content={"message": f"Oops! {exc.name} did something. There goes a rainbow..."},
+    )
+
+
+@app.get("/unicorns/{name}")
+async def read_unicorns(name: str):
+    if name == "yolo":
+        raise UnicornException(name=name)
+    return {"unicorn_name": name}
+
+
+@app.exception_handler(StarletteHTTPException)
+async def custom_http_exception_handler(request, exc):
+    print(f"OMG! An HTTP error!: {repr(exc)}")
+    return await http_exception_handler(request, exc)
+
+
+@app.exception_handler(RequestValidationError)
+async def validation_exception_handler(request, exc):
+    print(f"OMG! The client sent invalid data!: {exc}")
+    return await request_validation_exception_handler(request, exc)
+
+
+@app.get("/blah_items/{item_id}")
+async def read_items(item_id: int):
+    if item_id == 3:
+        raise HTTPException(status_code=418, detail="Nope! I don't like 3.")
+    return {"item_id": item_id}
+
